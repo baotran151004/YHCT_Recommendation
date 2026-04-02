0b3f3125b2afe2bd31a79106f060020f8641c813
baotran151004 <baotran.tomato151004@gmail.com>
2026-04-02 21:43:01 +0700

    changed

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001 >nul
 set PYTHONIOENCODING=utf-8
-npx -y concurrently --names "FastAPI,React" --prefix-colors "blue,green" "venv\Scripts\activate.bat && uvicorn main:app --reload" "cd yhct-frontend && npm start"
+npx -y concurrently --names "FastAPI,React" --prefix-colors "blue,green" "venv\Scripts\activate.bat && cd backend && uvicorn main:app --reload" "cd frontend && npm start"
